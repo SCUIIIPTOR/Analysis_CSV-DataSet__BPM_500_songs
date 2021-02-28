674603732d247265745e58694feab23854c92474
Oleg Larin <oleg.larin.2016@list.ru>
2021-02-28 21:17:25 +0300

    Analysis BPM
    Несколько лет назад, занимался изучением теории музыки, продавал и писал аудио-инструментал. Изначально, процесс явно творческий, но вскоре, мой интерес к коммерческой части превысил и возник вопрос: «В каком же темпе создавать ритм музыки?». Поэтому, идея анализа крупной выборки лучших композиций, для определения популярного [часто: самого продаваемого] диапазона темпа исполнения, не покидала с тех пор…

---
+++ Blues/Blues.py
@@ -0,0 +1,35 @@
+from matplotlib import pyplot as plt                   # Первый каноничный импорт
+import pandas as pd                                    # Второй каноничный импорт для обработки DataSet'а
+
+plt.style.use('fivethirtyeight')                       # Назначаем стилистику визуализации
+
+data_set = pd.read_csv('Blues.csv')                    # Считываем данные SCV-файла с DataSet'ом
+bpm = data_set['bpm']                                  # Переменная для параметра BPM в каждой строке
+year = data_set['year']                                # Переменная для параметра "год релиза" в каждой строке
+
+plt.scatter(                                           # Построение точечного графика и его настройка
+	bpm, year,
+	c=bpm,
+	s=bpm*1.5,
+	cmap='gist_heat',
+	edgecolor='black',
+	linewidth=.7
+)
+
+bar = plt.colorbar(                                    # Построение шкалы BPM
+			orientation='horizontal',
+			shrink=0.8,
+			extend='both',
+			extendfrac=.1
+)
+
+bar.set_label('Шкала ударов в минуту', fontsize=18)    # Подпись шкалы
+
+plt.title('Популярность скорости '                     # Заголовок графика
+		  'исполнения в блюзе', fontsize=25)
+
+plt.xlabel('BPM', fontsize=18)                         # Ось абсцисс
+plt.ylabel('Год релиза', fontsize=18)                  # Ось ординат
+
+plt.tight_layout()                                     # Настройка параметров подзаголовков в области отображения
+plt.show()                                             # Вывод на экран
---
+++ Rap/Rap end.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from collections import Counter
+
+plt.style.use("fivethirtyeight")
+
+data_set = pd.read_csv('Rap end.csv')
+index = data_set['number']
+ranges = data_set['bpm_range']
+
+counter = Counter()
+for index in ranges:
+	counter.update(index.split(';'))
+
+range_bpm = []
+value = []
+
+for item in counter.most_common(100):
+	range_bpm.append(item[0])
+	value.append(item[1])
+
+range_bpm.reverse()
+value.reverse()
+
+plt.barh(
+	range_bpm, value,
+	linewidth=.5,
+	edgecolor='black',
+	color='#e85b45',
+	label='Количество точек на предыдущем графике'
+)
+
+plt.legend()
+
+plt.title('Популярность интервала значений BPM в rap\'е', fontsize=25)
+plt.xlabel('Количество песен в диапазоне BPM', fontsize=18)
+plt.ylabel('Диапазоны BPM', fontsize=18)
+
+plt.tight_layout()
+plt.show()
