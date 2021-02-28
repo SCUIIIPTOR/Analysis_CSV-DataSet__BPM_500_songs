2266f46b1dff92cb1b8c5d64ddfd77ff975c0462
Олег Ларин <78920071+SCUIIIPTOR@users.noreply.github.com>
2021-02-28 20:28:08 +0200

    Update Blues.py

--- Blues/Blues.py
+++ Blues/Blues.py
@@ -1,13 +1,13 @@
-from matplotlib import pyplot as plt                   # Первый каноничный импорт
-import pandas as pd                                    # Второй каноничный импорт для обработки DataSet'а
+from matplotlib import pyplot as plt                               # Первый каноничный импорт
+import pandas as pd                                                     # Второй каноничный импорт для обработки DataSet'а
 
-plt.style.use('fivethirtyeight')                       # Назначаем стилистику визуализации
+plt.style.use('fivethirtyeight')                                          # Назначаем стилистику визуализации
 
-data_set = pd.read_csv('Blues.csv')                    # Считываем данные SCV-файла с DataSet'ом
-bpm = data_set['bpm']                                  # Переменная для параметра BPM в каждой строке
-year = data_set['year']                                # Переменная для параметра "год релиза" в каждой строке
+data_set = pd.read_csv('Blues.csv')                              # Считываем данные SCV-файла с DataSet'ом
+bpm = data_set['bpm']                                                   # Переменная для параметра BPM в каждой строке
+year = data_set['year']                                                   # Переменная для параметра "год релиза" в каждой строке
 
-plt.scatter(                                           # Построение точечного графика и его настройка
+plt.scatter(                                                                      # Построение точечного графика и его настройка
 	bpm, year,
 	c=bpm,
 	s=bpm*1.5,
@@ -16,7 +16,7 @@ plt.scatter(                                           # Построение т
 	linewidth=.7
 )
 
-bar = plt.colorbar(                                    # Построение шкалы BPM
+bar = plt.colorbar(                                                            # Построение шкалы BPM
 			orientation='horizontal',
 			shrink=0.8,
 			extend='both',
@@ -25,11 +25,11 @@ bar = plt.colorbar(                                    # Построение ш
 
 bar.set_label('Шкала ударов в минуту', fontsize=18)    # Подпись шкалы
 
-plt.title('Популярность скорости '                     # Заголовок графика
+plt.title('Популярность скорости '                                   # Заголовок графика
 		  'исполнения в блюзе', fontsize=25)
 
-plt.xlabel('BPM', fontsize=18)                         # Ось абсцисс
-plt.ylabel('Год релиза', fontsize=18)                  # Ось ординат
+plt.xlabel('BPM', fontsize=18)                                          # Ось абсцисс
+plt.ylabel('Год релиза', fontsize=18)                                # Ось ординат
 
-plt.tight_layout()                                     # Настройка параметров подзаголовков в области отображения
-plt.show()                                             # Вывод на экран
+plt.tight_layout()                                                               # Настройка параметров подзаголовков в области отображения
+plt.show()                                                                         # Вывод на экран
